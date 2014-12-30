d46ea5942db4152d5949612a18ddaa6844a520d0
xcxooxl <dudinirgad@Gmail.com>
2014-12-30 14:02:23 +0200

    Quinn V2.1

--- FogOfWarDetector/FogOfWarDetector/Properties/AssemblyInfo.cs
+++ FogOfWarDetector/FogOfWarDetector/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Quinn")]
+[assembly: AssemblyTitle("FogOfWarDetector")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Quinn")]
+[assembly: AssemblyProduct("FogOfWarDetector")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("64073475-895e-466d-9d30-c6b91fdf201e")]
+[assembly: Guid("e74ea0a4-9567-43bd-8bef-659697b08242")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
